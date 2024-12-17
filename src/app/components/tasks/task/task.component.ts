bba254b1b5de2bcdeb8c3ef5ab8a9be613b50f25
ThoRoro49 <rodolpheadabouko@gmail.com>
2024-12-17 11:21:16 +0100

    Implémentation de TaskComponent

---
+++ src/app/components/tasks/task/task.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Task } from '../../../models/task';
+
+@Component({
+  selector: 'app-task',
+  standalone: false,
+  
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+  @Input() tasks!:Task[]
+}
